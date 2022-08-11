bf57f99207be4fe3a79e8fa73d8a507a5409a6d4
Michael Fuchs <michael@derfuchs.net>
2022-08-11 11:08:11 +0200

    [feature] actual master node list in header tabs

--- src/components/layout/AppHeader/MasterNodeTabs.vue
+++ src/components/layout/AppHeader/MasterNodeTabs.vue
@@ -1,25 +1,24 @@
 <template>
-	<q-tabs
-		align="left"
-		dense
-		inline-label
-		outside-arrows
-		mobile-arrows
-		shrink
-		stretch
-	>
-		<q-route-tab to="/page1" label="MN 1" />
-		<q-route-tab to="/page2" label="MN 2" />
-		<q-route-tab to="/page3" label="MN 3" />
-		<q-route-tab to="/page3" label="MN 4" />
-		<q-route-tab to="/page3" label="MN 5" />
-	</q-tabs>
+  <q-tabs align="left" dense inline-label outside-arrows mobile-arrows shrink stretch>
+    <q-tab label="all" />
+    <q-tab v-for="mn in user.watchedActiveMasterNodes" :key="mn.id" :label="mn.name" />
+    <!--
+    <q-route-tab to="/page2" label="MN 2" />
+    -->
+  </q-tabs>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
+
+import { useUserStore } from "stores/user";
 
 export default defineComponent({
-	name: "MasterNodeTabs",
+  name: "MasterNodeTabs",
+  setup() {
+    return {
+      user: useUserStore(),
+    };
+  },
 });
 </script>
